378a9bfc5e14d6d67d144e34268075977755b4bc
lucas <lucaswtds@gmail.com>
2024-01-04 19:41:08 -0300

    fix: gamecard styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,9 +7,9 @@ interface GameCardProps {
 export function GameCard(props: GameCardProps) {
   return (
     <a href="" className="relative rounded-lg overflow-hidden">
-      <img src={props.bannerUrl} alt="" />
+      <img src={props.bannerUrl} alt="" className="w-full" />
 
-      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+      <div className="w-full pt-44 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">
           {props.title}
         </strong>
