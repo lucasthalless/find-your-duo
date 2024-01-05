4cc4f123434f1a91f0ab0fb8d44f268bc28f400f
lucas <lucaswtds@gmail.com>
2024-01-05 12:07:28 -0300

    refact: change web application path

--- web/src/components/SelectInput.tsx
+++ web/src/components/SelectInput.tsx
@@ -13,26 +13,27 @@ export function SelectInput({
   name
 }:SelectInputProps)  {
   return (
-    <Select.Root name={name}>
+    <Select.Root name={name} defaultValue="">
       <Select.Trigger
-        className="bg-zinc-900 py-3 px-4 rounded text-sm inline-flex items-center justify-between hover:bg-neutral-900 outline-none w-full"
+        className="bg-zinc-900 py-3 px-4 rounded text-sm flex items-center justify-between hover:bg-neutral-900 outline-none"
+        aria-label=""
       >
         <Select.Value placeholder={placeholder ? placeholder : "Selecione"} />
         <Select.Icon>
           <CaretDown className="w-6 h-6 caret-zinc-400" />
         </Select.Icon>
       </Select.Trigger>
-      <Select.Content className="bg-zinc-900 rounded text-sm flex items-center justify-between cursor-default w-full">
+      <Select.Content className="bg-zinc-900 rounded text-sm flex items-center justify-between cursor-default w-full p-2">
 
         <Select.ScrollUpButton />
-        <Select.Viewport className="p-2 flex w-full">
+        <Select.Viewport className="flex w-full">
           <Select.Group className="w-full">
             {items.map((item, key) => {
               return (
                 <Select.Item
                   value={item.id}
                   key={key}
-                  className="select-none rounded hover:bg-violet-500 p-1 flex items-center relative"
+                  className="select-none rounded hover:bg-violet-500 p-1 flex items-center"
                 >
                   <Select.ItemText>{item.title}</Select.ItemText>
                   <Select.ItemIndicator className="left-0 w-[25px] inline-flex items-center justify-center">
