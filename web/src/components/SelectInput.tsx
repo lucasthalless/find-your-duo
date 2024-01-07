8de0ab0eabfc8c62ef8922ae38b56addb05f0538
lucas <lucaswtds@gmail.com>
2024-01-06 23:18:31 -0300

    fix: adjust select content position

--- web/src/components/SelectInput.tsx
+++ web/src/components/SelectInput.tsx
@@ -13,17 +13,19 @@ export function SelectInput({
   name
 }:SelectInputProps)  {
   return (
-    <Select.Root name={name} defaultValue="">
+    <Select.Root name={name}>
       <Select.Trigger
         className="bg-zinc-900 py-3 px-4 rounded text-sm flex items-center justify-between hover:bg-neutral-900 outline-none"
-        aria-label=""
       >
         <Select.Value placeholder={placeholder ? placeholder : "Selecione"} />
         <Select.Icon>
           <CaretDown className="w-6 h-6 caret-zinc-400" />
         </Select.Icon>
       </Select.Trigger>
-      <Select.Content className="bg-zinc-900 rounded text-sm flex items-center justify-between cursor-default w-full p-2">
+      <Select.Content
+        position="popper"
+        className="bg-zinc-900 rounded text-sm flex items-center justify-between cursor-default w-full p-2"
+      >
 
         <Select.ScrollUpButton />
         <Select.Viewport className="flex w-full">
